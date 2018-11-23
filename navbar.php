cbf297d88dd0a5295195819e61f280eb52147416
FedeeMorales <fedeemorales21@gmail.com>
2018-11-23 16:08:55 -0300

    navbar mult usua

--- navbar.php
+++ navbar.php
@@ -3,25 +3,67 @@
 session_start();
 if(isset($_SESSION['nombre'])){
   if($_SESSION['nombre']!=""){
-  $nav='
-    <ul id="dropdown1" class="dropdown-content">
-        <li><a href="#!">one</a></li>
-        <li><a href="#!">two</a></li>
-        <li class="divider"></li>
-        <li><a href="cerrar_session.php">Cerrar Sesion</a></li>
-    </ul>
-    <div class="navbar-fixed ">
-    <nav class="indigo">
-    
-      <div class="nav-wrapper">
-        <a href="#!" class="brand-logo"><i class="fab fa-studiovinari fa-2x"></i></a>
-        <ul class="right hide-on-med-and-down">
-            <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">'.$_SESSION['nombre'].'<i class="material-icons right">arrow_drop_down</i></a></li>
-         </ul>
-      </div>
+    if ($_SESSION['perfil']=='g') {
+     $nav='
+      <ul id="dropdown1" class="dropdown-content">
+          <li><a href="#!">one</a></li>
+          <li><a href="#!">two</a></li>
+          <li class="divider"></li>
+          <li><a href="cerrar_session.php">Cerrar Sesion</a></li>
+      </ul>
+      <div class="navbar-fixed ">
+      <nav class="indigo">
       
-    </nav>
-  </div>';
+        <div class="nav-wrapper">
+          <a href="#!" class="brand-logo"><i class="fab fa-studiovinari fa-2x"></i></a>
+          <ul class="right hide-on-med-and-down">
+              <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">'.$_SESSION['nombre'].'<i class="material-icons right">arrow_drop_down</i></a></li>
+          </ul>
+        </div>
+        
+      </nav>
+    </div>';
+    }elseif ($_SESSION['perfil']=='p') {
+      $nav='
+      <ul id="dropdown1" class="dropdown-content">
+          <li><a href="altacurso.php">Agregar Curso</a></li>
+          <li><a href="reportes.php">Reportes</a></li>
+          <li class="divider"></li>
+          <li><a href="cerrar_session.php">Cerrar Sesion</a></li>
+      </ul>
+      <div class="navbar-fixed ">
+      <nav class="indigo">
+      
+        <div class="nav-wrapper">
+          <a href="#!" class="brand-logo"><i class="fab fa-studiovinari fa-2x"></i></a>
+          <ul class="right hide-on-med-and-down">
+              <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">'.$_SESSION['nombre'].'<i class="material-icons right">arrow_drop_down</i></a></li>
+          </ul>
+        </div>
+        
+      </nav>
+    </div>';
+    }elseif ($_SESSION['perfil']=='a') {
+      $nav='
+      <ul id="dropdown1" class="dropdown-content">
+          <li><a href="adm_usuarios.php">Adm. Usuarios</a></li>
+          <li><a href="adm_cursos.php">Adm. Cursos</a></li>
+          <li class="divider"></li>
+          <li><a href="cerrar_session.php">Cerrar Sesion</a></li>
+      </ul>
+      <div class="navbar-fixed ">
+      <nav class="indigo">
+      
+        <div class="nav-wrapper">
+          <a href="#!" class="brand-logo"><i class="fab fa-studiovinari fa-2x"></i></a>
+          <ul class="right hide-on-med-and-down">
+              <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">'.$_SESSION['nombre'].'<i class="material-icons right">arrow_drop_down</i></a></li>
+          </ul>
+        </div>
+        
+      </nav>
+    </div>';
+    }
   }
 
 }else {
@@ -38,8 +80,5 @@ if(isset($_SESSION['nombre'])){
       
     </nav>
   </div>';
-
-
-  
 }
   ?>
