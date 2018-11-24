db442ccbe5055ef4a1b6a3fb21e3674263896e4d
FedeeMorales <fedeemorales21@gmail.com>
2018-11-24 19:01:09 -0300

    estilos1

--- footer.php
+++ footer.php
@@ -6,11 +6,13 @@
             <div class="row">
               
               <div class="col s12 center-align">
-                <a class="white-text" href="#"><i class="fab fa-facebook-f"></i></a>
-                <a class="white-text" href="#"><i class="fab fa-instagram"></i></a>
-                <a class="white-text section" href="#"><i class="fab fa-twitter"></i></a>
+                <div class="redes">
+                  <a class="white-text" href="#"><i class="fab fa-facebook-f"></i></a>
+                  <a class="white-text" href="#"><i class="fab fa-instagram"></i></a>
+                  <a class="white-text section" href="#"><i class="fab fa-twitter"></i></a>
+                </div>
                 <div class="divider"></div>
-                <a href="contacto.php" class="white-text section">Otras Consultas</a>
+                <a href="contacto.php" class="white-text section contacto">Otras Consultas</a>
               </div>
             </div>
           </div>
