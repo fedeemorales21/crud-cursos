f85bf5197dbd4770316f034c2cef39e87417e8e4
FedeeMorales <fedeemorales21@gmail.com>
2018-11-27 13:50:51 -0300

    cacheo err

--- borrarcurso.php
+++ borrarcurso.php
@@ -1,16 +1,58 @@
-<?php
+
+
+ <!DOCTYPE html>
+<html>
+
+<head>
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+  <!--Import materialize.css-->
+  <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+  <link rel="stylesheet" href="css/style.css">
+
+  <!--Let browser know website is optimized for mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>ERROR</title>
+</head>
+
+<body>
+
+  <?php include "navbar.php"; ?>
+  <?=$nav?>
+
+  <main class="container">
+    
+  <?php
   include 'conexion.php';
   $id = $_GET['id'];
-  $base->query("DELETE FROM cursos WHERE curso_cod = '$id'");
-  
-  // $arr = $base->query("SELECT preg_nro FROM preguntas WHERE curso_cod = '$id'")->fetchAll(PDO::FETCH_OBJ);
-
-  //   foreach ($arr as $pregunta) {
+  try {
+    $base->query("DELETE FROM cursos WHERE curso_cod = '$id'");
     
-  //     echo $pregunta->preg_nro;
-
-  //   }    
-
-  header('Location:altacurso.php');
-  //solucionar tama con fk
+    header('Location:altacurso.php');
+   
+    
+  }  catch (\Exception $e) {
+    echo "<div class='red section center-align'><h1 class='center white-text'>ERROR</h1>
+    <br><h4 class='center white-text'>No siga intentando</h4>
+    <a class='center' href='altacurso.php'><h3>Volver</h3></a></div>";
+  }
  ?>
+      
+
+    
+
+  </main>
+
+  <?php include "footer.php" ?>
+  <?=footer()?>
+
+  <!--JavaScript at end of body for optimized loading-->
+  <script type="text/javascript" src="js/materialize.min.js"></script>
+  <script>
+     M.AutoInit();
+  </script>
+  
+</body>
+
+</html>
--- borrarpreg.php
+++ borrarpreg.php
@@ -1,8 +1,56 @@
-<?php
+ <!DOCTYPE html>
+<html>
+
+<head>
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+  <!--Import materialize.css-->
+  <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+  <link rel="stylesheet" href="css/style.css">
+
+  <!--Let browser know website is optimized for mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>ERROR</title>
+</head>
+
+<body>
+
+  <?php include "navbar.php"; ?>
+  <?=$nav?>
+
+  <main class="container">
+    
+  <?php
   include 'conexion.php';
   $id = $_GET['id'];
-  $base->query("DELETE FROM preguntas WHERE preg_nro = '$id'");
+  try {
+    $base->query("DELETE FROM preguntas WHERE preg_nro = '$id'");
 
-  header('Location:altapregunta.php');
-  //solucionar tama con fk
+    header('Location:altapregunta.php');
+   
+    
+  }  catch (\Exception $e) {
+    echo "<div class='red section center-align'><h1 class='center white-text'>ERROR</h1>
+    <br><h4 class='center white-text'>No siga intentando</h4>
+    <a class='center' href='altapregunta.php'><h3>Volver</h3></a></div>";
+  }
  ?>
+      
+
+    
+
+  </main>
+
+  <?php include "footer.php" ?>
+  <?=footer()?>
+
+  <!--JavaScript at end of body for optimized loading-->
+  <script type="text/javascript" src="js/materialize.min.js"></script>
+  <script>
+     M.AutoInit();
+  </script>
+  
+</body>
+
+</html>
