86ecdf12c020e8bb623d6bdfe34c8ed968e43191
FedeeMorales <fedeemorales21@gmail.com>
2018-11-30 20:30:25 -0300

    base de datos

--- trf.sql
+++ trf.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-11-2018 a las 19:43:36
+-- Tiempo de generación: 01-12-2018 a las 00:28:34
 -- Versión del servidor: 10.1.36-MariaDB
 -- Versión de PHP: 7.2.11
 
@@ -41,7 +41,7 @@ CREATE TABLE `cursos` (
 --
 
 INSERT INTO `cursos` (`curso_cod`, `curso_nombre`, `curso_profesor`, `curso_desc`, `curso_fecha`) VALUES
-(2, 'JS', 'fede moral', 'En este curso aprenderás a. Programar con JavaScript; Comprender la arquitectura básica de un programa. Trabajar con eventos. Armar funciones.', '2019-04-23'),
+(2, 'JS', 'fede moral', 'En este curso aprender&aacute;s a. Programar con JavaScript; Comprender la arquitectura b&aacute;sica de un programa. Trabajar con eventos. Armar funciones.', '2019-04-23'),
 (3, 'CSS', 'fede moral', 'En este curso aprenderemos gratis a maquetar sitios Web con HTML5 y a brindar estilos con CSS. No necesitas conocimientos previos, pues empezaremos', '2019-04-25'),
 (7, 'adccc', 'liz lisa', 'aaaaaaaaaaaaaaa', '2018-11-14');
 
@@ -66,7 +66,8 @@ INSERT INTO `curso_alumno` (`curso_nro`, `alumno_nro`) VALUES
 (7, 8),
 (2, 10),
 (3, 10),
-(2, 13);
+(2, 13),
+(7, 10);
 
 -- --------------------------------------------------------
 
@@ -94,7 +95,11 @@ INSERT INTO `encuesta` (`curso_cod`, `alumno_nro`, `preg_nro`, `rta`, `observaci
 (2, 13, 19, '5', NULL),
 (2, 13, 20, NULL, 'eeeeeee'),
 (3, 8, 5, '2', NULL),
-(3, 8, 22, '3', NULL);
+(3, 8, 22, '3', NULL),
+(7, 8, 23, '5', NULL),
+(2, 10, 3, '5', NULL),
+(2, 10, 19, '1', NULL),
+(2, 10, 20, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -138,9 +143,14 @@ CREATE TABLE `personas` (
 INSERT INTO `personas` (`cod_persona`, `nombre`, `apellido`, `dni`, `telefono`, `email`, `pass`, `tipo`) VALUES
 (5, 'fede', 'moral', 123, 44444444, 'f@hotmail.com', '$2y$10$IXDAndz.QYpBIjrg2h9FDO.8d7/BVo9wWubVkW2XfkZXtnzLtcO.i', 'p'),
 (6, 'liz', 'lisa', 111, 111, 'liz@hotmail.com', '$2y$10$za9LB9pHVdzOvJAokEAE7.ym08HZXpaM.x2jKWyVms6C2Ztfv.JrC', 'a'),
-(8, 'Loberto', 'Abielto', 123, 123, 'yo@gmail.com', '$2y$10$dkZqH2bEQod2u1BpN1rtCeoHoxT/TiRr5uGdZCGW9InoYK4twhf5K', 'g'),
+(8, 'Roberto', 'Lopez', 123, 123, 'yo@gmail.com', '$2y$10$dkZqH2bEQod2u1BpN1rtCeoHoxT/TiRr5uGdZCGW9InoYK4twhf5K', 'g'),
 (10, 'fdfdsfsd', 'fsddsfsd', 1233, 44444444, 'ffff@hotmail.com', '$2y$10$uPMmratXRC8mAemUg/HeDOPpJLv1HfbpwdO/G0x0qDmWwr9/3686O', 'g'),
-(13, 'rodolfo', 'depa', 123456789, 111111111, 'rododepa@hotmail.com', '$2y$10$Jr2nIVwXlsbUuvjJ5oV5LuR1h/2qPJqXwnvfTOfFMRMaUu/fIINwC', 'p');
+(13, 'rodolfo', 'depa', 123456789, 111111111, 'rododepa@hotmail.com', '$2y$10$Jr2nIVwXlsbUuvjJ5oV5LuR1h/2qPJqXwnvfTOfFMRMaUu/fIINwC', 'p'),
+(15, 'Federico', 'Morales', 38231553, 2147483647, 'fedeemorales21@gmail.com', '$2y$10$ShgOBzlAtdmKToII3WOT5uYU.8fixSgNhtOH4vQfEqOcmPoF6Z.Au', 'a'),
+(25, 'Atilio', 'Gonzalez', 38230962, 123456789, 'ati.gonzalez27@gmail.com', '$2y$10$1tEJsdvz9EY4C69cfYeR1OpG0sqSybldK9J0VqE9mB7fAzo.95oYG', 'g'),
+(26, 'Usuario', 'Administrador', 12345678, 123456789, 'administrador@gmail.com', '$2y$10$h5BjYoSNF0iLgSkh0.g0A.1ZdSxnCtZu4ZGIRJ5Sp9JmSyU9g6s6C', 'a'),
+(27, 'Usuario', 'Profesor', 12345678, 123456789, 'profesor@gmail.com', '$2y$10$XHH9oSoUlOaklGpx2u2YTeY9hNej/bzaJaw5KEInPEPVoslg1YnpO', 'p'),
+(28, 'Usuario', 'Alumno', 12345678, 123456789, 'alumno@gmail.com', '$2y$10$lZuxG9c91AhN5g2YUF5MFeYnKSpveztt0RWpMTo1awJmF5j./upom', 'g');
 
 -- --------------------------------------------------------
 
@@ -246,7 +256,7 @@ ALTER TABLE `tipo_pregunta`
 -- AUTO_INCREMENT de la tabla `cursos`
 --
 ALTER TABLE `cursos`
-  MODIFY `curso_cod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `curso_cod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `encuesta_obs`
@@ -258,7 +268,7 @@ ALTER TABLE `encuesta_obs`
 -- AUTO_INCREMENT de la tabla `personas`
 --
 ALTER TABLE `personas`
-  MODIFY `cod_persona` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `cod_persona` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT de la tabla `preguntas`
